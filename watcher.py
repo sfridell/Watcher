b7b8deb35851750e5073ba8e1d4056f536595909
SF <sfridell@system76-pc.localdomain>
2025-11-30 17:39:14 -0500

    Re-arrange arg hierarchy and add some pretty printing
    "dhcp" is now top level command with two sub-commands:
    "clients" and "static-leases".
    
    Use 'tabulate' to pretty print results.

--- watcher.py
+++ watcher.py
@@ -6,20 +6,31 @@ import argparse
 import sys
 import io
 import fabric
+from tabulate import tabulate
 
-def list_clients(args, connections, output):
+def list_dhcp_clients(args, connections, output):
     '''Connect to named router and list the connected clients
     '''
     conn = connections.get_connection(args.connection, output)
-    result = conn.run('cat /tmp/dnsmasq.leases')
-
-def list_static_leases(args, connections, output):
+    result = conn.run('cat /tmp/dnsmasq.leases', hide=True)
+    data = []
+    for line in result.stdout.splitlines():
+        data.append(line.split()[:-1])
+    headers = ['Expiration', 'MAC', 'IP', 'Hostname']
+    print(tabulate(data, headers=headers, tablefmt='simple'), file=output)
+    
+def list_dhcp_static_leases(args, connections, output):
     '''Connect to named router and list the configured static dhcp
        leases
     '''
     conn = connections.get_connection(args.connection, output)
-    result = conn.run('nvram show | grep static_leases')
-
+    result = conn.run('nvram show | grep static_leases', hide=True)
+    data = []
+    for lease in result.stdout.removeprefix('static_leases=').split():
+        data.append(lease.split('=')[:-1])
+    headers = ['MAC', 'Hostname', 'IP']
+    print(tabulate(data, headers=headers, tablefmt='simple'), file=output)
+    
 def get_args(argv):
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
@@ -34,12 +45,12 @@ def get_args(argv):
     parser_connections_new.add_argument("--username", type=str)
     parser_connections_new.add_argument("--pw", type=str)
 
-    parser_clients = subparsers.add_parser('clients')
-    parser_clients_commands = parser_clients.add_subparsers(dest='clients_command')
-    parser_clients_list = parser_clients_commands.add_parser('list')
-    parser_clients_list.add_argument("--connection", type=str)
-    parser_clients_list = parser_clients_commands.add_parser('static-leases')
-    parser_clients_list.add_argument("--connection", type=str)
+    parser_dhcp = subparsers.add_parser('dhcp')
+    parser_dhcp_commands = parser_dhcp.add_subparsers(dest='dhcp_command')
+    parser_dhcp_clients = parser_dhcp_commands.add_parser('clients')
+    parser_dhcp_clients.add_argument("--connection", type=str)
+    parser_dhcp_leases = parser_dhcp_commands.add_parser('static-leases')
+    parser_dhcp_leases.add_argument("--connection", type=str)
     
     return parser.parse_args(argv)
 
@@ -51,11 +62,11 @@ def process_command(argv = sys.argv[1:]):
     args = get_args(argv)
     output = io.StringIO()
     
-    if args.command == 'clients':
-        if args.clients_command == 'list':
-            list_clients(args, connections, output)
-        if args.clients_command == 'static-leases':
-            list_static_leases(args, connections, output)
+    if args.command == 'dhcp':
+        if args.dhcp_command == 'clients':
+            list_dhcp_clients(args, connections, output)
+        if args.dhcp_command == 'static-leases':
+            list_dhcp_static_leases(args, connections, output)
     if args.command == 'connections':
         if args.connections_command == 'list':
             connections.list_connections(output)
